b255cf98b80201e64cc146fe6724da290ceba89c
garciaveravictor <71645624+garciaveravictor@users.noreply.github.com>
2021-03-18 10:22:31 +0100

    Commit Inicial

---
+++ JUnit-Sample/test/sample/TestSuite.java
@@ -0,0 +1,24 @@
+package sample;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Victor Garcia Vera 2DAM
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({sample.VectorsTest.class, sample.UtilsTest.class})
+public class TestSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+}
