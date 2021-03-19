16337bdc27066752e49945141ced56401dc32889
garciaveravictor <71645624+garciaveravictor@users.noreply.github.com>
2021-03-19 14:51:19 +0100

    metodologia CORRECTO

---
+++ JUnit-Sample/src/sample/MasGrande.java
@@ -0,0 +1,19 @@
+package sample;
+
+/**
+ * @author: Profesora Devolver el elemento más grande de un Array
+ * @param a array de int
+ * @return elemento más grande de a
+ */
+public class MasGrande {
+
+    public static int max(int[] a) {
+        int i, m = 0;
+        for (i = 1; i < a.length; i++) {
+            if (a[i] > m) {
+                m = a[i];
+            }
+        }
+        return m;
+    }
+}
---
+++ JUnit-Sample/test/sample/MasGrandeTest.java
@@ -0,0 +1,30 @@
+package sample;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Victor Garcia Vera 2DAM
+ */
+public class MasGrandeTest {
+    
+    public MasGrandeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
